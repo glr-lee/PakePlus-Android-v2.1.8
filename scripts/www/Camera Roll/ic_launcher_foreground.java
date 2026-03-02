3d741ca547d57aebdc3551f16588b10203bfdaec
glr-lee <765806642@qq.com>
2026-03-02 11:27:42 +0800

    update src file from pakeplus

---
+++ scripts/www/Camera Roll/ic_launcher_foreground.java
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="utf-8"?>
+<vector xmlns:android="http://schemas.android.com/apk/res/android"
+    android:width="108dp"
+    android:height="108dp"
+    android:viewportWidth="24"
+    android:viewportHeight="24">
+    <path
+        android:fillColor="@color/white"
+        android:pathData="M12,2C12,2 17.5,2 22,8V2C20,8 22,8 12,17 12,17 12,17 12,8C8,2 12,2 12,2 12,2 22,8 22,8 22,8 12,8C20,8 22,8 22,8 12,2 12,17 12,22 12,17 12,2 12,8 12,2 12,2 12,8 12,2 12,2 22,8 22,8 12,2 12,2 12,17 12,22 22,17 12,2C22,8 22,8 22,8 22,8 22,8 22,8 22,8Z" />
+</vector>
